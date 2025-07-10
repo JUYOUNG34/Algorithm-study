2f46d02290d2b3e0c404c50e89a6ce81651e38a2
juyoung <juyoung34@soongsil.ac.kr>
2025-07-10 23:38:48 +0900

    [실버4] BOJ18110

---
+++ src/실버4/BOJ18110.java
@@ -0,0 +1,29 @@
+package 실버4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ18110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(A);
+        int M = (int)Math.round(N*0.15);
+
+        int sum = 0;
+        for(int i=M; i<N-M; i++){
+            sum += A[i];
+        }
+
+        float E = sum/(float)(N-2*M);
+        System.out.println(Math.round(E));
+    }
+}
