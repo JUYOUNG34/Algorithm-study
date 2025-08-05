a532e2bcef0a0c6dce49fecf3cc4181822424797
juyoung <juyoung34@soongsil.ac.kr>
2025-08-05 22:26:46 +0900

    [실버5] BOJ9655

---
+++ src/실버5/BOJ9655.java
@@ -0,0 +1,28 @@
+package 실버5;
+
+import java.util.Scanner;
+
+public class BOJ9655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int D[] = new int[1001];
+        D[1] = 1;
+        D[2] = 0;
+        D[3] = 1;
+
+        for(int i = 4; i <= N; i++){
+            if(D[i-1] == 1 || D[i-3] == 1){
+                D[i] = 0;
+            } else {
+                D[i] = 1;
+            }
+        }
+
+        if(D[N] == 1){
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
