61fad310859b1d348f00727aa33a5aad6b8f54ea
juyoung <juyoung34@soongsil.ac.kr>
2025-08-10 19:03:36 +0900

    [실버4] BOJ2217

---
+++ src/실버4/BOJ2217.java
@@ -0,0 +1,24 @@
+package 실버4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2217 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(A);
+        int max = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            int k = N - i;
+            max = Math.max(max, A[i] * k);
+        }
+        System.out.println(max);
+    }
+}
