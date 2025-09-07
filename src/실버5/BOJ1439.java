92a66e904f60882b37b2f07f76192a640953d419
juyoung <juyoung34@soongsil.ac.kr>
2025-09-07 14:51:12 +0900

    [실버5]BOJ1439

---
+++ src/실버5/BOJ1439.java
@@ -0,0 +1,30 @@
+package 실버5;
+
+import java.io.*;
+
+public class BOJ1439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int zero = 0;
+        int one = 0;
+
+        if (s.charAt(0) == '0') {
+            zero++;
+        } else {
+            one++;
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                if (s.charAt(i) == '0') {
+                    zero++;
+                } else {
+                    one++;
+                }
+            }
+        }
+
+        System.out.println(Math.min(zero, one));
+    }
+}
